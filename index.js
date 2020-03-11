5f47ea420e83a0329673a713a9700f65962c1550
Lhourquin <lhourquin@gmail.com>
2020-03-11 23:12:45 +0100

    add comment on js code and correction id name

--- index.js
+++ index.js
@@ -1,41 +1,57 @@
-let connection = document.getElementById('connection');
-let makeConection = document.getElementById('makeConection');
-let containerFormInscription = document.getElementById('container-form-inscription');
-let md = window.matchMedia("(max-width: 991px)");
-let buttonInscriptionMobile = document.getElementById('buttonInscriptionMobile');
-let welcomeToTheForum = document.getElementById('welcomeToTheForum');
-let crossInscription = document.getElementById('cross-form-inscription');
+// Target element
+const connection = document.getElementById('connection');
+const makeConnection = document.getElementById('makeConnection');
+const containerFormInscription = document.getElementById('container-form-inscription');
+const md = window.matchMedia("(max-width: 991px)");
+const buttonInscriptionMobile = document.getElementById('buttonInscriptionMobile');
+const welcomeToTheForum = document.getElementById('welcomeToTheForum');
+const crossInscription = document.getElementById('cross-form-inscription');
 
-
+// Event displaying form conection and close form connection
 connection.addEventListener('click', (e) => {
-    let target = e.target;
+    const target = e.target;
 
     if(target == connection){
-        makeConection.insertAdjacentHTML('afterbegin', '<div id="form-conection"><i id="cross-form-connection" class="fas fa-times cross-for-connection"></i><form><div class="form-group"><label for="exampleInputEmail1">Email address</label><input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" required><small id="emailHelp" class="form-text text-muted">We\'ll never share your email with anyone else.</small></div><div class="form-group"><label for="exampleInputPassword1">Password</label><input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" required></div><div class="form-check"><input type="checkbox" class="form-check-input" id="exampleCheck1"><label class="form-check-label" for="exampleCheck1">Check me out</label></div><button type="submit" class="btn btn-primary">Submit</button></form></div>');
+
+        // Add the form connection to the div empty "makeConnection"
+        makeConnection.insertAdjacentHTML('afterbegin', '<div id="form-conection"><i id="cross-form-connection" class="fas fa-times cross-for-connection"></i><form><div class="form-group"><label for="exampleInputEmail1">Email address</label><input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" required><small id="emailHelp" class="form-text text-muted">We\'ll never share your email with anyone else.</small></div><div class="form-group"><label for="exampleInputPassword1">Password</label><input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" required></div><div class="form-check"><input type="checkbox" class="form-check-input" id="exampleCheck1"><label class="form-check-label" for="exampleCheck1">Check me out</label></div><button type="submit" class="btn btn-primary">Submit</button></form></div>');
     
     }
     
-    let crossConnection = document.getElementById('cross-form-connection');
+    // Target the cross for close the form connection
+    const crossConnection = document.getElementById('cross-form-connection');
     
+    // Event for close the form connection
     crossConnection.addEventListener('click', (e)=>{
-        let target = e.target;
+        const target = e.target;
     
         if(target == crossConnection){
+
+            // Remove the form connection on div empty "makeConnection"
             crossConnection.parentElement.remove();
         }
     });
 
 });
 
+// Event responsive displaying of element
 function mediaScreen (mq) {
+
+    // Condition for changes displaying with media query arguments
     if(mq.matches){
+
+        // Element that changes without a particular event //
+
+        // Element display none
         containerFormInscription.style.display ="none";
+
+        // Element display
         buttonInscriptionMobile.style.display = "inline";
         crossInscription.style.display ='inline';
 
-
+        // Event displaying the form and close the welcome/description block
         buttonInscriptionMobile.addEventListener('click', (e)=>{
-            let target = e.target;
+            const target = e.target;
 
    
             if(target == buttonInscriptionMobile){
@@ -46,8 +62,9 @@ function mediaScreen (mq) {
    
         })
 
+        // Event close the form and display the welcome/description block
         crossInscription.addEventListener('click', (e)=>{
-            let target = e.target;
+            const target = e.target;
 
    
             if(target == crossInscription){
@@ -58,10 +75,14 @@ function mediaScreen (mq) {
    
         })
     }else{
+        // Element that changes without a particular event //
+
+        // Element display
         containerFormInscription.style.display ="inline";
+
+        // Element display none
         buttonInscriptionMobile.style.display = "none";
         crossInscription.style.display ='none';
-
         welcomeToTheForum.classList.remove('displayNone');  
 
     }
