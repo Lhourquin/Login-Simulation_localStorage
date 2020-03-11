a72a557c6a760e499cb491b8b664f728cae00ef4
lhourquin <lhourquin@gmail.com>
2020-03-11 15:05:50 +0100

    base of the index html with form inscription start

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let conection = document.getElementById('conection');
 let makeConection = document.getElementById('makeConection');
+
 conection.addEventListener('click', (e) => {
     let target = e.target;
 
@@ -7,8 +8,9 @@ conection.addEventListener('click', (e) => {
         makeConection.insertAdjacentHTML('afterbegin', '<div id="form-conection"><i id="cross-form-connection" class="fas fa-times cross-to-the-right"></i><form><div class="form-group"><label for="exampleInputEmail1">Email address</label><input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" required><small id="emailHelp" class="form-text text-muted">We\'ll never share your email with anyone else.</small></div><div class="form-group"><label for="exampleInputPassword1">Password</label><input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" required></div><div class="form-check"><input type="checkbox" class="form-check-input" id="exampleCheck1"><label class="form-check-label" for="exampleCheck1">Check me out</label></div><button type="submit" class="btn btn-primary">Submit</button></form></div>');
     
     }
+    
     let cross = document.getElementById('cross-form-connection');
-
+ 
     cross.addEventListener('click', (e)=>{
         let target = e.target;
     
