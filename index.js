a657dd10b1c3a30c31a6901c16ccee2ad422eccf
Lhourquin <lhourquin@gmail.com>
2020-03-11 22:48:09 +0100

    event responsive for index html done

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-let conection = document.getElementById('conection');
+let connection = document.getElementById('connection');
 let makeConection = document.getElementById('makeConection');
 let containerFormInscription = document.getElementById('container-form-inscription');
 let md = window.matchMedia("(max-width: 991px)");
 let buttonInscriptionMobile = document.getElementById('buttonInscriptionMobile');
 let welcomeToTheForum = document.getElementById('welcomeToTheForum');
+let crossInscription = document.getElementById('cross-form-inscription');
 
 
-conection.addEventListener('click', (e) => {
+connection.addEventListener('click', (e) => {
     let target = e.target;
 
-    if(target == conection){
-        makeConection.insertAdjacentHTML('afterbegin', '<div id="form-conection"><i id="cross-form-connection" class="fas fa-times cross-to-the-right"></i><form><div class="form-group"><label for="exampleInputEmail1">Email address</label><input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" required><small id="emailHelp" class="form-text text-muted">We\'ll never share your email with anyone else.</small></div><div class="form-group"><label for="exampleInputPassword1">Password</label><input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" required></div><div class="form-check"><input type="checkbox" class="form-check-input" id="exampleCheck1"><label class="form-check-label" for="exampleCheck1">Check me out</label></div><button type="submit" class="btn btn-primary">Submit</button></form></div>');
+    if(target == connection){
+        makeConection.insertAdjacentHTML('afterbegin', '<div id="form-conection"><i id="cross-form-connection" class="fas fa-times cross-for-connection"></i><form><div class="form-group"><label for="exampleInputEmail1">Email address</label><input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" required><small id="emailHelp" class="form-text text-muted">We\'ll never share your email with anyone else.</small></div><div class="form-group"><label for="exampleInputPassword1">Password</label><input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" required></div><div class="form-check"><input type="checkbox" class="form-check-input" id="exampleCheck1"><label class="form-check-label" for="exampleCheck1">Check me out</label></div><button type="submit" class="btn btn-primary">Submit</button></form></div>');
     
     }
     
-    let cross = document.getElementById('cross-form-connection');
- 
-    cross.addEventListener('click', (e)=>{
+    let crossConnection = document.getElementById('cross-form-connection');
+    
+    crossConnection.addEventListener('click', (e)=>{
         let target = e.target;
     
-        if(target == cross){
-            cross.parentElement.remove();
+        if(target == crossConnection){
+            crossConnection.parentElement.remove();
         }
     });
 
@@ -30,25 +31,42 @@ function mediaScreen (mq) {
     if(mq.matches){
         containerFormInscription.style.display ="none";
         buttonInscriptionMobile.style.display = "inline";
+        crossInscription.style.display ='inline';
 
+
+        buttonInscriptionMobile.addEventListener('click', (e)=>{
+            let target = e.target;
+
+   
+            if(target == buttonInscriptionMobile){
+            welcomeToTheForum.classList.add('displayNone');  
+            containerFormInscription.style.display ="inline";
+
+         }
+   
+        })
+
+        crossInscription.addEventListener('click', (e)=>{
+            let target = e.target;
+
+   
+            if(target == crossInscription){
+            welcomeToTheForum.classList.remove('displayNone');  
+            containerFormInscription.style.display ="none";
+
+         }
+   
+        })
     }else{
         containerFormInscription.style.display ="inline";
         buttonInscriptionMobile.style.display = "none";
+        crossInscription.style.display ='none';
+
+        welcomeToTheForum.classList.remove('displayNone');  
+
     }
 }
 
 mediaScreen(md);
 
 md.addListener(mediaScreen);
-
-buttonInscriptionMobile.addEventListener('click', (e)=>{
-    let target = e.target;
-
-    if(md.matches){
-        if(target == buttonInscriptionMobile){
-        welcomeToTheForum.style.visibility = 'hidden';  
-        }
-    }else{
-        welcomeToTheForum.style.setProperty('visibility' ,'visible');
-        }
-})
