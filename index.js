14e5e26cd8c0e025af895fe639961a1dcd2e0952
lhourquin <lhourquin@gmail.com>
2020-03-13 10:42:30 +0100

    start the login conexion

--- index.js
+++ index.js
@@ -3,185 +3,190 @@ const md = window.matchMedia("(max-width: 991px)");
 
 ///////////////////////////////////////////////////////////////
 ////////////////////// INDEX HTML /////////////////////////////
-if (window.location.href.indexOf("index") != -1){
+	if (window.location.href.indexOf("index") != -1){
 
-// Target element
-const connection = document.getElementById('connection');
-const makeConnection = document.getElementById('makeConnection');
-const containerFormInscription = document.getElementById('container-form-inscription');
-const buttonInscriptionMobile = document.getElementById('buttonInscriptionMobile');
-const welcomeToTheForum = document.getElementById('welcomeToTheForum');
-const crossInscription = document.getElementById('cross-form-inscription');
+		// Target element
+		const connection = document.getElementById('connection');
+		const makeConnection = document.getElementById('makeConnection');
+		const containerFormInscription = document.getElementById('container-form-inscription');
+		const buttonInscriptionMobile = document.getElementById('buttonInscriptionMobile');
+		const welcomeToTheForum = document.getElementById('welcomeToTheForum');
+		const crossInscription = document.getElementById('cross-form-inscription');
 
-// Event displaying form conection and close form connection
+		// Event displaying form conection and close form connection
 
 
-    connection.addEventListener('click', (e) => {
-        const target = e.target;
-    
-        if(target == connection){
-    
-            // Add the form connection to the div empty "makeConnection"
-            makeConnection.insertAdjacentHTML('afterbegin', '<div id="form-conection"><i id="cross-form-connection" class="fas fa-times cross-for-connection"></i><form><div class="form-group"><label for="exampleInputEmail1">Email address</label><input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" required><small id="emailHelp" class="form-text text-muted">We\'ll never share your email with anyone else.</small></div><div class="form-group"><label for="exampleInputPassword1">Password</label><input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" required></div><div class="form-check"><input type="checkbox" class="form-check-input" id="exampleCheck1"><label class="form-check-label" for="exampleCheck1">Check me out</label></div><button type="submit" class="btn btn-primary">Submit</button></form></div>');
-        
-        }
-        
-        // Target the cross for close the form connection
-        const crossConnection = document.getElementById('cross-form-connection');
-        
-        // Event for close the form connection
-        crossConnection.addEventListener('click', (e)=>{
-            const target = e.target;
-        
-            if(target == crossConnection){
-    
-                // Remove the form connection on div empty "makeConnection"
-                crossConnection.parentElement.remove();
-            }
-        });
-    
-    });
+		connection.addEventListener('click', (e) => {
+			const target = e.target;
 
-    function mediaScreen (mq) {
-        // Condition for changes displaying with media query arguments
-        if(mq.matches){
-    
-            // Element that changes without a particular event //
-    
-            // Element display none
-            containerFormInscription.style.display ="none";
-    
-            // Element display
-            buttonInscriptionMobile.style.display = "inline";
-            crossInscription.style.display ='inline';
-    
-            // Event displaying the form and close the welcome/description block
-            buttonInscriptionMobile.addEventListener('click', (e)=>{
-                const target = e.target;
-    
-       
-                if(target == buttonInscriptionMobile){
-                welcomeToTheForum.classList.add('displayNone');  
-                containerFormInscription.style.display ="inline";
-    
-             }
-       
-            })
-    
-            // Event close the form and display the welcome/description block
-            crossInscription.addEventListener('click', (e)=>{
-                const target = e.target;
-    
-       
-                if(target == crossInscription){
-                welcomeToTheForum.classList.remove('displayNone');  
-                containerFormInscription.style.display ="none";
-    
-             }
-       
-            })
-        }else{
-            // Element that changes without a particular event //
-    
-            // Element display
-            containerFormInscription.style.display ="inline";
-    
-            // Element display none
-            buttonInscriptionMobile.style.display = "none";
-            crossInscription.style.display ='none';
-            welcomeToTheForum.classList.remove('displayNone');  
-    
-        }
-    }
-    
-    mediaScreen(md);
-    
-    md.addListener(mediaScreen);
+			if(target == connection){
 
-}
+				// Add the form connection to the div empty "makeConnection"
+				makeConnection.insertAdjacentHTML('afterbegin', '<div id="form-conection"><i id="cross-form-connection" class="fas fa-times cross-for-connection"></i><form action="forum.html"><div class="form-group"><label for="inputEmail">Email address</label><input type="email" class="form-control" id="inputEmail" name="inputEmail" aria-describedby="emailHelp" placeholder="Enter email" required><small id="emailHelp" class="form-text text-muted">We\'ll never share your email with anyone else.</small></div><div class="form-group"><label for="inputPassword">Password</label><input type="password" class="form-control" id="inputPassword" name="inputPassword" placeholder="Password" required></div><div class="form-check"><input type="checkbox" class="form-check-input" id="exampleCheck1"><label class="form-check-label" for="exampleCheck1">Check me out</label></div><button type="submit" id="submitConection" class="btn btn-primary">Submit</button></form></div>');
+
+			}
+
+			// Target the cross for close the form connection
+			const crossConnection = document.getElementById('cross-form-connection');
+
+			// Event for close the form connection
+			crossConnection.addEventListener('click', (e)=>{
+				const target = e.target;
+
+				if(target == crossConnection){
+
+					// Remove the form connection on div empty "makeConnection"
+					crossConnection.parentElement.remove();
+				}
+			});
+
+		});
+
+		function mediaScreen (mq) {
+			// Condition for changes displaying with media query arguments
+			if(mq.matches){
+
+				// Element that changes without a particular event //
+
+				// Element display none
+				containerFormInscription.style.display ="none";
+
+				// Element display
+				buttonInscriptionMobile.style.display = "inline";
+				crossInscription.style.display ='inline';
+
+				// Event displaying the form and close the welcome/description block
+				buttonInscriptionMobile.addEventListener('click', (e)=>{
+					const target = e.target;
+
+
+					if(target == buttonInscriptionMobile){
+						welcomeToTheForum.classList.add('displayNone');  
+						containerFormInscription.style.display ="inline";
+
+					}
+
+				})
+
+				// Event close the form and display the welcome/description block
+				crossInscription.addEventListener('click', (e)=>{
+					const target = e.target;
+
+
+					if(target == crossInscription){
+						welcomeToTheForum.classList.remove('displayNone');  
+						containerFormInscription.style.display ="none";
+
+					}
+
+				})
+			}else{
+				// Element that changes without a particular event //
+
+				// Element display
+				containerFormInscription.style.display ="inline";
+
+				// Element display none
+				buttonInscriptionMobile.style.display = "none";
+				crossInscription.style.display ='none';
+				welcomeToTheForum.classList.remove('displayNone');  
+
+			}
+		}
+
+		mediaScreen(md);
+
+		md.addListener(mediaScreen);
+
+	}
 
 ////////////////////// INDEX HTML /////////////////////////////
 ///////////////////////////////////////////////////////////////
 
 
-///////////////////////////////////////////////////////////////
-///////////////////// FORUM HTML //////////////////////////////
+	///////////////////////////////////////////////////////////////
+	///////////////////// FORUM HTML //////////////////////////////
 
-if (window.location.href.indexOf("forum") != -1){
-// Target element
-let deconnection = document.getElementById('deconnection');
-let userProfile = document.getElementById('userProfile');
-let userProfilePseudo = document.getElementById('userProfilePseudo');
-let userProfileMail = document.getElementById('userProfileMail');
-let userProfileAge = document.getElementById('userProfileAge');
-let userProfileCity = document.getElementById('userProfileCity');
-let dateOfConection = document.getElementById('dateOfConection');
-let userIcon = document.getElementById('userIcon');
-let actuality = document.getElementById('actuality');
-const crossProfile = document.getElementById('cross-for-profile');
-
-
-
-//////////////////////////////////////////////////////////////
-//////////////////// Global function for all file ////////////
-
-// Event responsive displaying of element
-
-function mediaScreen (mq) {
-    // Condition for changes displaying with media query arguments
-    if(mq.matches){
-
-        // Element that changes without a particular event //
-
-        // Element display none
-        userProfile.style.display ="none";
-        crossProfile.style.display ="inline";
-        // Element display
-        
-
-        // Event displaying the profile and close the actuality block
-        userIcon.addEventListener('click', (e)=>{
-            const target = e.target;
-
-            if(mq.matches){
-                if(target == userIcon){
-                    userProfile.style.display ="inline";
-                    actuality.style.display = "none";
-            }
-            
-         }
-   
-        });
+	if (window.location.href.indexOf("forum") != -1){
+		// Target element
+		let deconnection = document.getElementById('deconnection');
+		let userProfile = document.getElementById('userProfile');
+		let userProfilePseudo = document.getElementById('userProfilePseudo');
+		let userProfileMail = document.getElementById('userProfileMail');
+		let userProfileAge = document.getElementById('userProfileAge');
+		let userProfileCity = document.getElementById('userProfileCity');
+		let dateOfConection = document.getElementById('dateOfConection');
+		let userIcon = document.getElementById('userIcon');
+		let actuality = document.getElementById('actuality');
+		const crossProfile = document.getElementById('cross-for-profile');
 
 
-        // Event close the profile and display the actuality block
-        
-        crossProfile.addEventListener('click', (e)=>{
-            const target = e.target;
 
-   
-            if(target == crossProfile){
-                actuality.style.display = "inline";
-                userProfile.style.display ="none";
+		//////////////////////////////////////////////////////////////
+		//////////////////// Global function for all file ////////////
 
-         }
-   
-        })
-    }else{
+		// Event responsive displaying of element
 
-        // Element display
-        userProfile.style.display ="inline";
-        actuality.style.display = "inline";
- 
-        // Element display none
-        crossProfile.style.display ="none";
-    }
-}
+		function mediaScreen (mq) {
+			// Condition for changes displaying with media query arguments
+			if(mq.matches){
 
-mediaScreen(md);
+				// Element that changes without a particular event //
 
-md.addListener(mediaScreen);
-}
+				// Element display none
+				userProfile.style.display ="none";
+				crossProfile.style.display ="inline";
+				// Element display
+
+
+				// Event displaying the profile and close the actuality block
+				userIcon.addEventListener('click', (e)=>{
+					const target = e.target;
+
+					if(mq.matches){
+						if(target == userIcon){
+							userProfile.style.display ="inline";
+							actuality.style.display = "none";
+						}
+
+					}
+
+				});
+
+
+				// Event close the profile and display the actuality block
+
+				crossProfile.addEventListener('click', (e)=>{
+					const target = e.target;
+
+
+					if(target == crossProfile){
+						actuality.style.display = "inline";
+						userProfile.style.display ="none";
+
+					}
+
+				})
+			}else{
+
+				// Element display
+				userProfile.style.display ="inline";
+				actuality.style.display = "inline";
+
+				// Element display none
+				crossProfile.style.display ="none";
+			}
+		}
+
+		mediaScreen(md);
+
+		md.addListener(mediaScreen);
+
+		deconnection.addEventListener('click', ()=> {
+			sessionStorage.removeItem('userSession');
+			window.location.href = "index.html";
+		})
+	}
 
 ///////////////////// FORUM HTML //////////////////////////////
 ///////////////////////////////////////////////////////////////
--- login.js
+++ login.js
@@ -1,20 +1,87 @@
-let formDataInscription = document.getElementById('formDataInscription');
+if (window.location.href.indexOf("index") != -1){
 
-const userOrder = {};
+	//Inscription
+	let formDataInscription = document.getElementById('formDataInscription');
 
-function getValues(e) {
-  // turn form elements object into an array
-  const elements = Array.prototype.slice.call(e.target.elements);
 
-  // go over the array storing input name & value pairs
-  elements.forEach((el) => {
-    if (el.type !== "submit") {
-      userOrder[el.name] = el.value;
-    }
-  });
+	const userData = {};
 
-  // finally save to localStorage
-  localStorage.setItem('userOrder', JSON.stringify(userOrder));
-}  
+	function getValues(e) {
+		// turn form elements object into an array
+		const elements = Array.prototype.slice.call(e.target.elements);
 
-formDataInscription.addEventListener("submit", getValues);
+		// go over the array storing input name & value pairs
+		elements.forEach((el) => {
+			if (el.type !== "submit"){
+				userData[el.name] = el.value;
+			}
+		});
+
+		// finally save to localStorage
+		sessionStorage.setItem('userSession', JSON.stringify(userData));
+		localStorage.setItem('userAccount',JSON.stringify(userData));
+	}  
+
+	formDataInscription.addEventListener("submit", getValues);
+
+	// Connection
+
+	let getData = JSON.parse(localStorage.getItem('userAccount'));
+
+
+	let formConection = document.getElementById('form-conection');
+
+	if(document.body.contains(formConection)){
+		console.log("is load");
+		let inputEmail = document.getElementById('inputEmail').value;
+		let inputPassword = document.getElementById('inputPassword').value;
+		let submitConection = document.getElementById('submitConection');
+		let arr = [];
+
+
+		for(let itm in getData){
+			arr.push(getData[itm]);
+		}
+
+		function checkAccount(){
+			if(inputEmail == arr[2]){
+				console.log(inputEmail);
+				if(inputPassword != arr[6]){
+					alert('wrong password !');
+				}else if (inputPassword == arr[6]){
+					sessionStorage.setItem('userSession', JSON.stringify(getData));
+				}
+			}
+
+			submitConection.addEventListener('click', checkAccount);
+		}
+	}
+
+
+
+
+}
+
+
+if (window.location.href.indexOf("forum") != -1){
+
+	let getData = JSON.parse(sessionStorage.getItem('userSession'));
+
+	let arr = [];
+
+	if(getData){
+		for(let itm in getData){
+			arr.push(getData[itm]);
+		}
+		userProfilePseudo.innerText = arr[0] + ' ' + arr[1];
+		userProfileMail.innerText = arr[2];
+		userProfileGender.innerText = arr[3];
+		userProfileCity.innerText = arr[4];
+		userprofileAge.innerText = arr[5] + ' years old';
+
+	}else{
+		window.location.href = "index.html";
+	}
+
+
+}
